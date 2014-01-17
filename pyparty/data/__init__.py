2f3775890b402f91859336cb0c3f5c35cc4a6252
hugadams <hugadams@gwmail.gwu.edu>
2014-01-17 01:32:16 -0500

    Adding a test data improt system

---
+++ pyparty/data/__init__.py
@@ -0,0 +1,38 @@
+# Import sample data.  This import structure was taken directly from skimages
+# data import style:
+   # https://github.com/scikit-image/scikit-image
+
+import os.path as op
+from skimage.io import imread
+from pyparty import data_dir
+
+
+__all__ = ['gwu', 
+           'spectrum',
+           'lena_who'
+           ]
+
+def load(f):
+    """Load an image file located in the data directory.
+
+    Parameters
+    ----------
+    f : string
+        File name.
+
+    Returns
+    -------
+    img : ndarray
+        Image loaded from skimage.data_dir.
+    """
+    return imread(op.join(data_dir, f))
+
+
+def gwu():
+    return load("gwu.jpg")
+
+def spectrum():
+    return load("spectrum.jpg")
+
+def lena_who():
+    return load("lena_who.jpg")
