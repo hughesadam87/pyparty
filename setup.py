f63b3eec5c0be155c1b80bc4cb084d076624dd0a
hugadams <hugadams@gwmail.gwu.edu>
2014-01-21 22:42:56 -0500

    readme change

--- setup.py
+++ setup.py
@@ -7,23 +7,16 @@ NAME = 'pyparty'
 
 # Python >= 2.7 ?
 # -------
-
 #user_py = sys.version_info
 #if user_py < (2, 7):
 #    raise SystemExit('%s requires python 2.7 or higher (%s found).' % \
-#                     (NAME, '.'.join(str(x) for x in user_py[0:3])))
-
-#XXX Get current module path and do path.join(requirements)
-#with open(op.join(sys.path[0], 'requirements.txt')) as f:
-with open('requirements.txt', 'r') as f:
-    required = f.readlines()
-    required = [line.strip() for line in required]
+#                    (NAME, '.'.join(str(x) for x in user_py[0:3])))
     
 # For now, most of these are in here for testing.  Dependency version 
 #requirements can probably be relaxed, especially chaco.
 setup(
     name = NAME,
-    version = '0.1.2-1',
+    version = '0.1.1-4', # NO HYPHENS
     author = 'Adam Hughes',
     maintainer = 'Adam Hughes',
     maintainer_email = 'hughesadam87@gmail.com',
@@ -33,7 +26,8 @@ setup(
     # include .tex and .ipynb files
     package_data={   
       'pyparty.examples.Notebooks':['*.ipynb'],
-      'pyparty.data':['*']
+      'pyparty.data':['*'],
+      'pyparty':['*.rst']
 #      'pyuvvis.scripts':['*.png']
                  },
        
@@ -42,27 +36,24 @@ setup(
 #                       'gwureport = pyuvvis.scripts.gwu_script.gwureport:main']
 #                    },
     
- #   url = 'http://pypi.python.org/pypi/PyUvVis/',
+    url = 'http://pypi.python.org/pypi/pyparty/',
     download_url = 'https://github.com/hugadams/pyparty',
     license = 'LICENSE.txt',
     description = 'Tools for patterning 2d-shapes on ndarrays',
-    long_description = open(op.join(sys.path[0], 'README.rst')).read(),
-    install_requires = [
-        # Setup.py install actually downloads and installs current version. 
-        # IS THAT DESIRABLE? Or not cool to put that here?
-        "scikit-image", 
-     	"traits"
-		],
+#    long_description = open(op.join(sys.path[0], 'README.rst'), 'rb').read(),
+    
     classifiers = [
           'Development Status :: 2 - Pre-Alpha',
           'Environment :: Console',
           'Intended Audience :: Science/Research',
           'Intended Audience :: Developers',
           'Natural Language :: English',
-#          'Operating System :: OSIndependent',  #This is limited only by 
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Operating System :: MacOS',          
           'Programming Language :: Python :: 2',
           'Topic :: Scientific/Engineering :: Visualization',
           'Topic :: Scientific/Engineering :: Physics',
-		  # ADD MORE?
           ],
 )
