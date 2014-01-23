f8e439bde79ca8a072f1864bd75c998a6d9179c4
hugadams <hugadams@gwmail.gwu.edu>
2014-01-23 17:17:18 -0500

    Removed build directory

--- build/lib/pyparty/bundled/traitlets/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    from _version import version as __version__
-except ImportError:
-    __version__ = "no-built"
-
-from ._implementation import HasTraits, MetaHasTraits, TraitType, Any, CBytes, Dict, \
-    Int, Long, Integer, Float, Complex, Bytes, Unicode, TraitError, \
-    Undefined, Type, This, Instance, TCPAddress, List, Tuple, \
-    ObjectName, DottedObjectName, CRegExp, Enum, Bool, CBool
--- build/lib/pyparty/shape_models/api.py
+++
@@ -1,9 +0,0 @@
-from shape_models.circles import Circle, Dimer
-
-PARTICLETYPES= \
-    {
-     'circle': Circle,
-     'dimer' : Dimer,
-     }
-
-# Eventually, break these up like "CIRCLETYPES", "RECTANGLES" etc...
