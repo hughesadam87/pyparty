2ed173e2fb7d9911172fd1c6e34ff88a28d17f9b
hugadams <hugadams@gwmail.gwu.edu>
2014-03-18 16:15:42 -0400

    Added multiplot

--- pyparty/plots.py
+++ pyparty/plots.py
@@ -2,3 +2,17 @@
 backwards compatibility by not changing utils.py"""
 
 from pyparty.utils import grayhist, splot, showim, zoom, zoomshow
+
+from pyparty.utils import multi_axes
+
+def multishow(images, *args, **kwargs):
+    if getattr(images, '__iter__'):
+        return showim(images, *args, **kwargs)
+
+    if len(images) == 1:
+        return showim(images[0], *args, **kwargs)
+    
+    axes, kwargs = multi_axes(len(images), **kwargs)
+    for idx, ax in enumerate(axes):
+        showim(images[0], ax, *args, **kwargs)
+    return axes
